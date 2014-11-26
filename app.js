60f8de64114304ec4bd87b192df0f1cdf0a46577
Leon Andrews <innerwestsoul@gmail.com>
2014-11-26 16:16:28 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express')
+var app = express();
+var port = process.env.PORT || 1337; // FOR AZURE
+var counter = 0;
+var pubnub = require("pubnub").init({
+    publish_key   : "pub-c-9339f7dd-e9a8-41d7-b3a4-037d25972fc2",
+    subscribe_key : "sub-c-34f9f230-6ef5-11e4-bcf0-02ee2ddab7fe"
+});
+
+/*  -------------------------  */
+/*  --  Stuff David Changed  --  */
+/*  -------------------------  */
+
+app.use(express.static(__dirname + '/client')); //IMO, leave the user-facing page as the default (less to type on phone), load the dashboard with /dashboard.html
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); //makes it easy to send JSON
+
+//Shuffle the array once (when the server starts). Then just serve sequentially to each new user.
+function shuffle(originalArray) {
+  var tmpCounter = originalArray.length
+    , tempVal
+    , i
+  ;
+  while (tmpCounter > 0) {
+    i = Math.floor(Math.random() * tmpCounter);
+    tmpCounter--;
+    tempVal = originalArray[tmpCounter];
+    originalArray[tmpCounter] = originalArray[i];
+    originalArray[i] = tempVal;
+  }
+  return originalArray;
+}
+
+var shuffledPostcodes = shuffle(require('./NSWLATLON.json'));
+
+//the code in client.js uses a 'get' request when the page loads.
+//this is a route that catches that 'get' request and response with something.
+//So in effect this is a function that runs for each new user/refresh, increments the counter and returns that item in the array.
+
+app.get('/api/getPostcode', function(req, res) {
+  counter++;
+  res.json(shuffledPostcodes[counter]);
+  // poss add WURFL here?
+});
+
+
+/*  -----------------------------  */
+/*  --  end Stuff David Added  --  */
+/*  -----------------------------  */
+
+var server = app.listen(1337, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('App listening at http://%s:%s', host, port)
+})
+
+console.log('Locations Built');
---
+++ client/client.js
@@ -0,0 +1,82 @@
+var IOTPostcode;
+function doSomethingInterestingWithPostcode(data) {
+  //This is such a lazy way to do this. Here you would call a function that does 
+  $('#postcode').text('Postcode: ' + data.postcode);
+  $('#suburb').text('Suburb: ' + data.suburb);
+  $('#lat').text('Lat: ' + data.lat);
+  $('#lon').text('Lon: ' + data.lon);
+
+  var lat = data.lat;
+  var lon = data.lon;
+
+}
+
+var Device = navigator.platform;
+
+
+var pubnub = PUBNUB.init({
+  publish_key   : "pub-c-9339f7dd-e9a8-41d7-b3a4-037d25972fc2",
+  subscribe_key : "sub-c-34f9f230-6ef5-11e4-bcf0-02ee2ddab7fe"
+});
+
+// subscribe to the channel, and render all messages as markers on the map
+function sendToPubNub(colour) {
+  IOTPostcode.colour = colour;
+  IOTPostcode.device = Device;
+  pubnub.publish({
+        channel: 'leontest1',
+        message: IOTPostcode
+     });
+  console.log('Will set postcode', IOTPostcode, 'to', colour);
+  //here, send something to pub nub (IOTPostcode) is the postcode object that will have been set by now.
+}
+
+function bindEvents() {
+  //Bind some events
+  $('#set-low').click(function() {
+    sendToPubNub('green');
+  });
+  $('#set-med').click(function() {
+    sendToPubNub('orange');
+  });
+  $('#set-high').click(function() {
+    sendToPubNub('red');
+  });
+
+}
+
+
+
+function renderMap(lat, lon) {
+  var map;
+      function initialize() {
+        var mapOptions = {
+          zoom: 8,
+          center: new google.maps.LatLng(lat,lon)
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize());
+      var LatLng = new google.maps.LatLng(lat, lon);
+        var myMarker = new google.maps.Marker({
+          position: LatLng,
+          animation: google.maps.Animation.DROP,
+          map: map});
+}
+
+
+
+
+//This runs when the page loads.
+//$.get makes an AJAX request to that URL. Node handles this with the app.get('/api/getPostcode'... code and returns a data object.
+$.get('/api/getPostcode', function(data, WURFL) {
+  console.log(data); //You can check this out in the Chrome console.
+  console.log("Browser Type = " + Device);
+  console.log("Data lat = " + data.lat);
+  IOTPostcode = data;
+  doSomethingInterestingWithPostcode(data);
+  renderMap(data.lat, data.lon);
+  bindEvents();
+});
+
+
