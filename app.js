23593e23a14e03d3c17e28d207c65c1f742d8e3d
vampirate <vmprt@Daniels-MacBook-Pro-3.local>
2018-09-10 14:37:49 +1000

    update

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var app = express();
 var port = process.env.PORT || 1337; // FOR AZURE
 var counter = 0;
 var pubnub = require("pubnub").init({
-    publish_key   : "pub-c-9339f7dd-e9a8-41d7-b3a4-037d25972fc2",
-    subscribe_key : "sub-c-34f9f230-6ef5-11e4-bcf0-02ee2ddab7fe"
+  publish_key: "pub-c-9339f7dd-e9a8-41d7-b3a4-037d25972fc2",
+  subscribe_key: "sub-c-34f9f230-6ef5-11e4-bcf0-02ee2ddab7fe"
 });
 
 /*  -------------------------  */
@@ -18,10 +18,8 @@ app.use(bodyParser.json()); //makes it easy to send JSON
 
 //Shuffle the array once (when the server starts). Then just serve sequentially to each new user.
 function shuffle(originalArray) {
-  var tmpCounter = originalArray.length
-    , tempVal
-    , i
-  ;
+  var tmpCounter = originalArray.length,
+    tempVal, i;
   while (tmpCounter > 0) {
     i = Math.floor(Math.random() * tmpCounter);
     tmpCounter--;
@@ -38,7 +36,7 @@ var shuffledPostcodes = shuffle(require('./NSWLATLON.json'));
 //this is a route that catches that 'get' request and response with something.
 //So in effect this is a function that runs for each new user/refresh, increments the counter and returns that item in the array.
 
-app.get('/api/getPostcode', function(req, res) {
+app.get('/api/getPostcode', function (req, res) {
   counter++;
   res.json(shuffledPostcodes[counter]);
   // poss add WURFL here?
--- client/client.js
+++ client/client.js
@@ -1,52 +1,56 @@
 var IOTPostcode;
+
 function doSomethingInterestingWithPostcode(data) {
   //This is such a lazy way to do this. Here you would call a function that does 
   $('#postcode').text('Postcode: ' + data.postcode);
   $('#suburb').text('Suburb: ' + data.suburb);
   $('#lat').text('Lat: ' + data.lat);
   $('#lon').text('Lon: ' + data.lon);
-  $('#location').text(data.suburb + ', NSW ' + data.postcode);
+  $('#location').text(data.suburb + ", NSW " + data.postcode);
 
   var lat = data.lat;
   var lon = data.lon;
-
 }
 
 var Device = navigator.platform;
+console.log("device is " + Device);
 
 var pubnub = PUBNUB.init({
-  publish_key   : "pub-c-9339f7dd-e9a8-41d7-b3a4-037d25972fc2",
-  subscribe_key : "sub-c-34f9f230-6ef5-11e4-bcf0-02ee2ddab7fe"
+  publish_key: "pub-c-9339f7dd-e9a8-41d7-b3a4-037d25972fc2",
+  subscribe_key: "sub-c-34f9f230-6ef5-11e4-bcf0-02ee2ddab7fe"
 });
 
 // subscribe to the channel, and render all messages as markers on the map
 function sendToPubNub(colour) {
-
-  if (colour != 'clear') { IOTPostcode.colour = colour; IOTPostcode.clear = '';}
-  else {IOTPostcode.clear = 'clear'; IOTPostcode.colour = '';}
-  //IOTPostcode.colour = colour;
+  if (colour != 'clear') {
+    IOTPostcode.colour = colour;
+    IOTPostcode.clear = '';
+  } else {
+    IOTPostcode.clear = 'clear';
+    IOTPostcode.colour = '';
+  }
   IOTPostcode.device = Device;
   pubnub.publish({
-        channel: 'leontest1',
-        message: IOTPostcode
-     });
+    channel: 'leontest1',
+    message: IOTPostcode
+  });
   console.log('Will set postcode', IOTPostcode, 'to', colour);
   //here, send something to pub nub (IOTPostcode) is the postcode object that will have been set by now.
 }
 
 function bindEvents() {
   //Bind some events
-  $('#set-low').click(function() {
+  $('#set-low').click(function () {
     sendToPubNub('green');
   });
-  $('#set-med').click(function() {
+  $('#set-med').click(function () {
     sendToPubNub('orange');
   });
-  $('#set-high').click(function() {
+  $('#set-high').click(function () {
     sendToPubNub('red');
-  });  
-  $('#set-clear').click(function() {
-    sendToPubNub('clear');  
+  });
+  $('#set-clear').click(function () {
+    sendToPubNub('clear');
   });
 
 }
@@ -55,20 +59,24 @@ function bindEvents() {
 
 function renderMap(lat, lon) {
   var map;
-      function initialize() {
-        var mapOptions = {
-          zoom: 7,
-          center: new google.maps.LatLng(lat,lon),
-          disableDefaultUI: true
-        };
-        map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize());
-      var LatLng = new google.maps.LatLng(lat, lon);
-        var myMarker = new google.maps.Marker({
-          position: LatLng,
-          animation: google.maps.Animation.DROP,
-          map: map});
+
+  function initialize() {
+    var mapOptions = {
+      zoom: 7,
+      center: new google.maps.LatLng(lat, lon),
+      disableDefaultUI: true
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize());
+  var LatLng = new google.maps.LatLng(lat, lon);
+  
+  var myMarker = new google.maps.Marker({
+    position: LatLng,
+    animation: google.maps.Animation.DROP,
+    map: map
+  });
 }
 
 
@@ -76,7 +84,7 @@ function renderMap(lat, lon) {
 
 //This runs when the page loads.
 //$.get makes an AJAX request to that URL. Node handles this with the app.get('/api/getPostcode'... code and returns a data object.
-$.get('/api/getPostcode', function(data, WURFL) {
+$.get('/api/getPostcode', function (data, WURFL) {
   console.log(data); //You can check this out in the Chrome console.
   //console.log("Browser Type = " + ua);
   console.log("Data lat = " + data.lat);
@@ -85,5 +93,3 @@ $.get('/api/getPostcode', function(data, WURFL) {
   renderMap(data.lat, data.lon);
   bindEvents();
 });
-
-
