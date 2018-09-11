1833e0101544232213f4fe3471d82ea2b82bc750
vampirate <vmprt@Daniels-MacBook-Pro-3.local>
2018-09-11 11:44:10 +1000

    overall cleaning

--- client/client.js
+++ client/client.js
@@ -1,41 +1,54 @@
 var IOTPostcode;
 
-function doSomethingInterestingWithPostcode(data) {
-  //This is such a lazy way to do this. Here you would call a function that does 
-  $('#postcode').text('Postcode: ' + data.postcode);
-  $('#suburb').text('Suburb: ' + data.suburb);
-  $('#lat').text('Lat: ' + data.lat);
-  $('#lon').text('Lon: ' + data.lon);
-  $('#location').text(data.suburb + ", NSW " + data.postcode);
+var pubnub = new PubNub({
+  publishKey: "pub-c-9339f7dd-e9a8-41d7-b3a4-037d25972fc2",
+  subscribeKey: "sub-c-34f9f230-6ef5-11e4-bcf0-02ee2ddab7fe",
+  ssl: true
+});
 
+//This runs when the page loads.
+//$.get makes an AJAX request to that URL. Node handles this with the app.get('/api/getPostcode'... code and returns a data object.
+$.get('/api/getPostcode', function (data, WURFL) {
+  console.log(data); //You can check this out in the Chrome console.
+  //console.log("Browser Type = " + ua);
+  console.log("Data lat = " + data.lat);
+  IOTPostcode = data;
   var lat = data.lat;
   var lon = data.lon;
-}
-
-var Device = navigator.platform;
-console.log("device is " + Device);
-
-var pubnub = PUBNUB.init({
-  publish_key: "pub-c-9339f7dd-e9a8-41d7-b3a4-037d25972fc2",
-  subscribe_key: "sub-c-34f9f230-6ef5-11e4-bcf0-02ee2ddab7fe"
+  renderMap(data.lat, data.lon);
+  bindEvents();
+  doSomethingInterestingWithPostcode(data);
 });
 
 // subscribe to the channel, and render all messages as markers on the map
 function sendToPubNub(colour) {
-  if (colour != 'clear') {
-    IOTPostcode.colour = colour;
-    IOTPostcode.clear = '';
-  } else {
-    IOTPostcode.clear = 'clear';
-    IOTPostcode.colour = '';
-  }
+  IOTPostcode.colour = colour;
   IOTPostcode.device = Device;
-  pubnub.publish({
-    channel: 'leontest1',
-    message: IOTPostcode
+
+  pubnub.subscribe({
+    channels: ['leontest1'],
   });
-  console.log('Will set postcode', IOTPostcode, 'to', colour);
-  //here, send something to pub nub (IOTPostcode) is the postcode object that will have been set by now.
+
+  pubnub.publish({
+      message: {
+        such: IOTPostcode
+      },
+      channel: 'leontest1',
+      sendByPost: false, // true to send via post
+      storeInHistory: false, //override default storage options
+      meta: {
+        "cool": "meta"
+      } // publish extra meta with the request
+    },
+    function (status, response) {
+      if (status.error) {
+        // handle error
+        console.log(status)
+      } else {
+        console.log("message Published, will set postcode", IOTPostcode, " to ", colour)
+      }
+    }
+  );
 }
 
 function bindEvents() {
@@ -53,7 +66,6 @@ function bindEvents() {
   $('#set-clear').click(function () {
     sendToPubNub('clear');
   });
-
 }
 
 function renderMap(lat, lon) {
@@ -67,10 +79,8 @@ function renderMap(lat, lon) {
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   }
-
   google.maps.event.addDomListener(window, 'load', initialize());
   var LatLng = new google.maps.LatLng(lat, lon);
-  
   var myMarker = new google.maps.Marker({
     position: LatLng,
     animation: google.maps.Animation.DROP,
@@ -78,17 +88,13 @@ function renderMap(lat, lon) {
   });
 }
 
+function doSomethingInterestingWithPostcode(data) {
+  $('#postcode').text('Postcode: ' + data.postcode);
+  $('#suburb').text('Suburb: ' + data.suburb);
+  $('#lat').text('Lat: ' + data.lat);
+  $('#lon').text('Lon: ' + data.lon);
+  $('#location').text(data.suburb + ", NSW " + data.postcode);
+}
 
-
-
-//This runs when the page loads.
-//$.get makes an AJAX request to that URL. Node handles this with the app.get('/api/getPostcode'... code and returns a data object.
-$.get('/api/getPostcode', function (data, WURFL) {
-  console.log(data); //You can check this out in the Chrome console.
-  //console.log("Browser Type = " + ua);
-  console.log("Data lat = " + data.lat);
-  IOTPostcode = data;
-  doSomethingInterestingWithPostcode(data);
-  renderMap(data.lat, data.lon);
-  bindEvents();
-});
+var Device = navigator.platform;
+console.log("The device sending the request is " + Device);
